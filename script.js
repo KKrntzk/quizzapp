c12849c4a77159792e4b326de8f76b6fd0af4f69
KKrntzk <sophie.kornetzki@gmail.com>
2025-11-25 10:23:17 +0100

    function init and showCurrentQuestion added

--- script.js
+++ script.js
@@ -58,3 +58,19 @@ let questions = [
     right_answer: 1,
   },
 ];
+
+let currentQuestion = 0;
+
+function init() {
+  document.getElementById("totalQuestionLength").innerHTML = questions.length;
+  showCurrentQuestion();
+}
+
+function showCurrentQuestion() {
+  let question = questions[currentQuestion];
+  document.getElementById("questionText").innerHTML = question["question"];
+  document.getElementById("answer_1").innerHTML = question["answer_1"];
+  document.getElementById("answer_2").innerHTML = question["answer_2"];
+  document.getElementById("answer_3").innerHTML = question["answer_3"];
+  document.getElementById("answer_4").innerHTML = question["answer_4"];
+}
